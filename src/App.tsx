e42ed12f74e1202ab1e39ef404d4189529949d7d
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-09-19 10:36:53 -0300

    Configurando o roteamento [React Router Dom] da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
 
+import { Router } from './Routes'
+
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Olá Mundo</h1>
-
       <GlobalStyle />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
---
+++ src/Routes.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { HomePage } from './pages/Home'
+import { CheckoutPage } from './pages/Checkout'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+    </Routes>
+  )
+}
