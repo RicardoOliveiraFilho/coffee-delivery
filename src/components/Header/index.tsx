77509fcf36291088c355b74d7bef5ba5505c5e71
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-09-19 12:34:09 -0300

    Criação do Componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
+
+import logoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <div className="container">
+        <img src={logoImg} alt="" />
+
+        <HeaderButtonsContainer>
+          <HeaderButton variant="purple">
+            <MapPin size={20} weight="fill" />
+            Porto Alegre, RS
+          </HeaderButton>
+
+          <HeaderButton variant="yellow">
+            <ShoppingCart size={20} weight="fill" />
+          </HeaderButton>
+        </HeaderButtonsContainer>
+      </div>
+    </HeaderContainer>
+  )
+}
